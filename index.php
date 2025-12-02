c81d85efee4ae500dbaa2030a09cb06d380779b0
superfeatz <hafidzharridil99@gmail.com>
2025-12-02 15:17:46 +0700

    fix source

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'db.php';
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>📰 News Portal Modern</title>
+  <title>📰 News Portal</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
